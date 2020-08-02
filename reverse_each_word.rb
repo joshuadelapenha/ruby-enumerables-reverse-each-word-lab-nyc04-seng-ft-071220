8d8c81b2c36fe07bc6d8b072016eda63395dcb3e
Joshua D <joshuadelapenha@yahoo.com>
2020-08-02 22:46:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(argument_sentence)
   argument_sentence = argument_sentence.split(" ")
   new_sentence = argument_sentence.map {|string| string.reverse}
-  argument_sentence.join(' ')
+  new_sentence.join(' ')
 end
